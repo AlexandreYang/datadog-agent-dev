a97215c80cedfc5c783ad9e81bc9a42151250094
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-05-05 19:18:01 +0200

    Update init script

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # datadog-agent-dev
 
+## Setup
+
 ```
 git clone https://github.com/DataDog/datadog-agent.git ./data
 vagrant up
+```
+
+## Workflow
+
+The `./data/datadog-agent` git repository is shared with the vagranat VM.
+
+It's mapped to `/home/vagrant/work/src/github.com/DataDog/datadog-agent`.
+
+
+## Useful Commmand
+
+```
+vagrant ssh
+
+invoke agent.build --build-exclude=systemd
 ```
--- init_script.sh
+++ init_script.sh
@@ -2,8 +2,8 @@ set -e
 
 export DEBIAN_FRONTEND=noninteractive
 export HOME=/home/vagrant
-export AGENT_REPO=$GOPATH/src/github.com/DataDog/datadog-agent
 export GOPATH=$HOME/work
+export AGENT_REPO=$GOPATH/src/github.com/DataDog/datadog-agent
 
 echo "Init Script"
 
@@ -28,7 +28,7 @@ sudo mv go /usr/local
 
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install -y python3.7 python3-pip python3-invoke
+sudo apt-get install -y python3.7-dev python3-pip python3-invoke
 
 # alias pip to pip3
 sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
@@ -48,10 +48,15 @@ sudo sh cmake-3.12.0-Linux-x86_64.sh --prefix=/usr/local --exclude-subdir
 cat << EOF >> $HOME/.profile
 export GOROOT=/usr/local/go
 export GOPATH=$GOPATH
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+export PATH=\$GOPATH/bin:\$GOROOT/bin:\$PATH
 
-cd $GOPATH/src/github.com/DataDog/datadog-agent
+PYTHON_INCLUDE_DIR=/usr/include/python2.7 
+PYTHON_LIBRARY=/usr/lib/python2.7/config/libpython2.7.so
+
+cd $AGENT_REPO
 EOF
+
+source $HOME/.profile
 
 # ==========================================
 # PERMISSIONS
@@ -61,14 +66,10 @@ sudo chown vagrant -R /home/vagrant/work
 # ==========================================
 # AGENT
 # ==========================================
+
 cd $AGENT_REPO
 pip3 install -r requirements.txt
 python3 -m invoke deps
-
-# ==========================================
-# AGENT COMMAND
-# ==========================================
-# python3 -m invoke agent.build
 
 # ==========================================
 # END
